61a9585f2ed3d59efec890d87b3ebaa8c9151995
HitechXXI <joao.pereira@alunos.ufersa.edu.br>
2021-04-02 19:47:48 -0300

    Tentando rodar o Index
    Inicialmente deu certo, mas agora está dando pau.  Está acusando o seguinte erro:
      Failed to execute goal org.apache.maven.plugins:maven-resources-plugin:3.2.0:resources (default-resources) on project cadpessoas: Input length = 1 -> [Help 1]

--- cadpessoas/src/main/java/br/com/joaopereira/cadpessoas/controllers/PessoaController.java
+++ cadpessoas/src/main/java/br/com/joaopereira/cadpessoas/controllers/PessoaController.java
@@ -1,11 +1,21 @@
 package br.com.joaopereira.cadpessoas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.joaopereira.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 
 public class PessoaController {
-
+	@Autowired
+	PessoaRepository pessoaRepo;
+	
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
 }
