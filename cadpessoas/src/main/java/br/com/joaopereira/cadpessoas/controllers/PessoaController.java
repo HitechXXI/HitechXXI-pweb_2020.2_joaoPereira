e8112624dbac873924d890bef7fb80b42281c023
HitechXXI <joao.pereira@alunos.ufersa.edu.br>
2021-04-10 20:24:20 -0300

    Avançando na elaboração do CRUD

--- cadpessoas/src/main/java/br/com/joaopereira/cadpessoas/controllers/PessoaController.java
+++ cadpessoas/src/main/java/br/com/joaopereira/cadpessoas/controllers/PessoaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,16 +18,28 @@ public class PessoaController {
 	@Autowired
 	PessoaRepository pessoaRepo;
 	
+	PessoaController(PessoaRepository pessoaRepo) {
+		this.pessoaRepo = pessoaRepo;
+	}
+	
 	@GetMapping
 	public String index() {
 		return "index.html";
 	}
 	
-	@GetMapping("/listarPessoa.html")
+	@GetMapping("/listarPessoas")
 	public ModelAndView listarPessoas() {
 		List<Pessoa> lista = pessoaRepo.findAll();
-		ModelAndView mav = new ModelAndView("listarPessoas");
-		mav.addObject("pessoas", lista);
-		return mav;
+		ModelAndView modelAndView = new ModelAndView("listarPessoas");
+		modelAndView.addObject("pessoas", lista);
+		return modelAndView;
+	}
+	
+	@GetMapping("/remover/{id}")
+	public ModelAndView removerPessoa(@PathVariable("id") long id) {
+		Pessoa aRemover = pessoaRepo.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("ID Inválido: " + id));	
+		pessoaRepo.delete(aRemover);
+		return new ModelAndView("redirect:listarPessoas");
 	}
 }
